eea04e5d1e81e28a8c8796bbe9357cb6e10f2e65
Pari <parnianshahla2004@gmail.com>
2024-02-29 16:01:48 +0330

    little changes

--- controllers/user.v1.controller.js
+++ controllers/user.v1.controller.js
@@ -2,7 +2,6 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import errorHandler from "../utils/error.js";
-import { json } from "express";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
