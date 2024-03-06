f473c9ea201b576a40e367c6ddc9170658f5e53d
Pari <parnianshahla2004@gmail.com>
2024-03-06 11:25:51 +0330

    vote functionality improved

--- controllers/vote.controller.js
+++ controllers/vote.controller.js
@@ -1,9 +1,11 @@
 import jwt from "jsonwebtoken";
 import { promisify } from "util";
+import { ObjectId } from "mongodb";
 import AppError from "../utils/appError.js";
 import { getDb } from "../db.js";
-import { ObjectId } from "mongodb";
+import logger from "../logger/logger.js";
 
+//! Refoctor - a function that returns the id of the user + the db
 export const sendUpvote = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
@@ -26,26 +28,32 @@ export const sendUpvote = async (req, res, next) => {
 
     res.status(201).json(newVote);
   } catch (error) {
+    logger.error(error);
     next(error);
   }
 };
 
 export const sendDownvote = async (req, res, next) => {
   try {
+
     let downvote;
     const token = req.headers.authorization.split(" ")[1];
     const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-    const db = getDb().db();
 
+    const db = getDb().db();
     const voted = await db.collection("votes").findOne({
-      feedbackId: new ObjectId(req.params.id),
-      upvotedBy: new ObjectId(decoded.id),
+      $and: [
+        { feedbackId: new ObjectId(req.params.id) },
+        { upvotedBy: new ObjectId(decoded._id) },
+      ],
     });
-    console.log(voted);
+
     if (voted) {
       downvote = await db.collection("votes").deleteOne({
-        feedbackId: new ObjectId(req.params.id),
-        upvotedBy: new ObjectId(decoded.id),
+        $and: [
+          { feedbackId: new ObjectId(req.params.id) },
+          { upvotedBy: new ObjectId(decoded._id) },
+        ],
       });
     } else {
       return next(new AppError("Can't take your vote twice!", 400));
@@ -53,6 +61,7 @@ export const sendDownvote = async (req, res, next) => {
 
     res.status(201).json(downvote);
   } catch (error) {
+    logger.error(error);
     next(error);
   }
 };
