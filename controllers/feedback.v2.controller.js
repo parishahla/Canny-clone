d7e80ac6f7188d13547f018a1343b794799fe6ad
Pari <parnianshahla2004@gmail.com>
2024-03-12 11:39:02 +0330

    Adapted to its corresponding repo

--- controllers/feedback.v2.controller.js
+++ controllers/feedback.v2.controller.js
@@ -1,11 +1,13 @@
 import Feedback from "../model/feedback.model.js";
 import errorHandler from "../utils/error.js";
 import AppError from "../utils/appError.js";
+import FeedbackRepository from "../repositories/feedback.repo.js";
 
 //* feedback routes
 export const getAllFeedback = async (req, res, next) => {
+  const feedbackRepo = new FeedbackRepository();
   try {
-    const allFeedback = await Feedback.find();
+    const allFeedback = await feedbackRepo.getAllFeedback();
     res.status(200).json(allFeedback);
   } catch (error) {
     next(error);
@@ -13,11 +15,12 @@ export const getAllFeedback = async (req, res, next) => {
 };
 
 export const getFeedback = async (req, res, next) => {
+  const feedbackRepo = new FeedbackRepository();
   try {
     //* MongoDB
-    const feedback = await Feedback.findById(req.params.id);
-    // same as => findOne({ _id: req.params.id})
+    const feedback = await feedbackRepo.getFeedbackById(req.params.id);
 
+    //! handle the wrong id in a another way
     // if (!feedback) {
     // return next(errorHandler(404, "No feedback with this ID"));
     //   return next(new AppError("No feedback with this ID", 404));
@@ -30,9 +33,10 @@ export const getFeedback = async (req, res, next) => {
 };
 
 export const sendFeedback = async (req, res, next) => {
+  const feedbackRepo = new FeedbackRepository();
   try {
-    //* Mongo DB
-    const newFeedback = await Feedback.create(req.body);
+    //! handle input
+    const newFeedback = await feedbackRepo.createFeedback(req.body);
     res.status(201).json(newFeedback);
   } catch (error) {
     next(error);
@@ -40,14 +44,11 @@ export const sendFeedback = async (req, res, next) => {
 };
 
 export const updateFeedback = async (req, res, next) => {
+  const feedbackRepo = new FeedbackRepository();
   try {
-    const updatedFeedback = await Feedback.findByIdAndUpdate(
+    const updatedFeedback = await feedbackRepo.updateFeedback(
       req.params.id,
       req.body,
-      {
-        new: true,
-        runValidators: true,
-      },
     );
     if (!updatedFeedback) {
       return next(errorHandler(404, "No feedback with this ID"));
@@ -59,9 +60,10 @@ export const updateFeedback = async (req, res, next) => {
 };
 
 export const deleteFeedback = async (req, res, next) => {
+  const feedbackRepo = new FeedbackRepository();
+
   try {
-    //* Mongo DB
-    const deletedFeedback = await Feedback.findByIdAndDelete(req.params.id);
+    const deletedFeedback = await feedbackRepo.deleteFeedback(req.params.id);
     if (!deletedFeedback) {
       return next(errorHandler(404, "No feedback found with this ID"));
     }
