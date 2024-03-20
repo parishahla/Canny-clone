d328677acb1bf500e57850248b2b4400f9719667
pari.dev <pari.dev@gmail.com>
2024-03-20 03:37:29 +0330

    Mail abstract class

--- utils/email.js
+++ utils/email.js
@@ -1,26 +1,31 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { createTransport } from "nodemailer";
+import logger from "../logger/logger.js";
 
-const sendEmail = async (options) => {
-  const transporter = createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    tls: true,
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASSWORD,
-    },
-  });
+class Mail {
+  createTransport() {
+    return createTransport({
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      tls: true,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    });
+  }
 
-  transporter
-    .sendMail({
-      from: "pari@e8i.ir",
-      to: options.email,
-      subject: options.subject,
-      text: options.message,
-    })
-    .then(() => console.log("OK, Email has been sent."))
-    .catch(console.error);
-};
-
-export default sendEmail;
+  sendEmail(email, subject, message) {
+    const transporter = this.createTransport();
+    transporter
+      .sendMail({
+        from: "pari@e8i.ir",
+        to: email,
+        subject: subject,
+        text: message,
+      })
+      .then(() => console.log("Email has been sent."))
+      .catch((err) => logger.error(err));
+  }
+}
+export default new Mail();
